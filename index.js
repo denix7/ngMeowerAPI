35868a3e44a078c9c043db8271cddffd4bca27d4
Dennis <dennis.brian.pv@gmail.com>
2018-04-30 16:51:16 -0400

    Conectando a BD

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict'
 
-console.log('Iniciando');
+var mongoose = require('mongoose');
+
+var port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/ngRedSocial', (err, res)=>{
+    if(err){
+        throw err;
+    }else{
+        console.log('La conexion a la BD fue exitosa');
+    }
+})
