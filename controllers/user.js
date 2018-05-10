ce35b9323b79ff9281e721381f3d0c397416ce84
Dennis <dennis.brian.pv@gmail.com>
2018-05-10 14:09:10 -0400

    Crando metodo para registrar usuarios en BD

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
 function home (req, res){
@@ -10,7 +10,40 @@ function pruebas (req, res){
     res.status(200).send({message: 'Accion pruebas its works!'});
 }
 
+function saveUser(req, res){
+    var user = new User();
+    var params = req.body;
+
+    if(params.name && params.surname && params.nick && params.email && params.password)
+    {
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        bcrypt.hash(params.password, null, null, (err, hash)=>{
+            user.password = hash;
+
+            user.save((err, userStored)=>{
+                if(err){
+                    return res.status(500).send({message: 'Error al registrar el usuario'});
+                }
+                else if(!userStored){
+                    res.status(404).send({message: 'No se ha registrado el usuario'});
+                }else{
+                    res.status(200).send({user: userStored});
+                }
+            });
+        });
+    }else{
+        res.status(200).send({message: 'Es necesario enviar todos los campos necesarios'});
+    }
+}
+
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 }
