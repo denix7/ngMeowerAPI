d2cda670ba9e3a01dcbb37872620924d9fea76c1
Dennis <dennis.brian.pv@gmail.com>
2018-05-15 00:38:24 -0400

    Creando metodo para seguir a usuario

--- controllers/follow.js
+++ controllers/follow.js
@@ -3,13 +3,32 @@
 var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
-var follow = require('../models/follow');
+var Follow = require('../models/follow');
 
 //Pruebas
 function prueba (req, res){
     res.status(200).send({message: 'Accion follow works!'});
 }
 
+//Seguir usuario
+function saveFollow(req, res){
+    var params = req.body;
+    var follow = new Follow();
+    
+    follow.user = req.user.sub;//id del usuario identificado
+    follow.followed = params.followed;//le paso por parametro el valor followed, id al que quiero seguir
+
+    follow.save((err, followStored)=>{
+        if(err)
+            return res.status(500).send({message: 'Error al guardar el seguimiento'});
+        else if(!followStored)
+            return res.status(404).send({message: 'El seguimiento no se ha guardado'});
+        else    
+            return res.status(200).send({follow: followStored});        
+    })
+}
+
 module.exports = {
-    prueba
+    prueba,
+    saveFollow
 }
