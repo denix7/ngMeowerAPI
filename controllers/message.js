42eaa8e755d2f04cd594e7ecab0fb6bd9b6d91c0
Dennis <dennis.brian.pv@gmail.com>
2018-05-23 14:08:57 -0400

    Creando metodo para enviar mensaje

--- controllers/message.js
+++ controllers/message.js
@@ -10,6 +10,29 @@ function probandoMessage(req, res){
     res.status(200).send({message: 'Controller message its works!'});
 }
 
+function saveMessage(req, res){
+    var params = req.body;
+
+    if(!params.text || !params.receiver)
+        return res.status(200).send({message: 'Ingrese todos los campos requeridos'});
+    
+    var message = new Message(); 
+    message.emmiter = req.user.sub;
+    message.receiver = params.receiver;
+    message.text = params.text;
+    message.created_at = moment().unix();  
+    
+    message.save((err, messageStored)=>{
+        if(err)
+            return res.status(500).send({message: 'Error en la peticion'});
+        else if(!messageStored)
+            return res.status(404).send({message: 'No existe el mensaje'});
+        else    
+            return res.status(200).send({message: messageStored}); 
+    })
+}
+
 module.exports = {
-    probandoMessage
+    probandoMessage,
+    saveMessage
 }
--- routes/message.js
+++ routes/message.js
@@ -6,5 +6,6 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/prueba-message', MessageController.probandoMessage);
+api.post('/message',md_auth.ensureAuth, MessageController.saveMessage);
 
 module.exports = api
